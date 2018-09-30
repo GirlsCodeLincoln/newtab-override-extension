070213cdd2785c53987b6650e697e4a1476f8a0d
troynguyen8@gmail.com <troynguyen8@gmail.com>
2018-09-30 13:00:30 -0500

    split logic to get date string into separate function

--- newPage.js
+++ newPage.js
@@ -1,3 +1,15 @@
+function getDateString() {
+  var date = new Date();
+  var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+  var newDate = new Date(utc + (3600000 * -5));
+
+  var timeString = newDate.toTimeString();
+  timeString = timeString.replace(/GMT.+/g, '');
+
+  return newDate.toDateString() + "\n" + timeString;
+}
+
 var backgrounds = [
   "https://i.redditmedia.com/45JCXfJd9UKnGMIVkJ9bzCxAafcpXbAN3FwR3lF1FLM.jpg?s=5e876b86af49dd430e8c37f4ff93fdb9",
   "https://i.redditmedia.com/rF-A898kd0gQfaZ9vrPakO6g45quAzZ4bVpQH4k1PzY.png?s=a7a40456b7c56b7d066b0ea8c6909746",
@@ -13,12 +25,4 @@ document.body.style.backgroundSize = "cover";
 
 var timeElement = document.getElementById("current-date");
 
-var date = new Date();
-var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
-
-var newDate = new Date(utc + (3600000 * -5));
-
-var timeString = newDate.toTimeString();
-timeString = timeString.replace(/GMT.+/g, '');
-
-timeElement.innerHTML = newDate.toDateString() + "\n" + timeString;
+timeElement.innerHTML = getDateString();
